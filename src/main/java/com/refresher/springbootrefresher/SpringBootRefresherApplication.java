f94107fdefbe6359878e50072f18b3db66801564
BertramSu <bxs442@case.edu>
2024-01-10 02:40:25 -0600

    Initializr Skeleton

---
+++ src/main/java/com/refresher/springbootrefresher/SpringBootRefresherApplication.java
@@ -0,0 +1,13 @@
+package com.refresher.springbootrefresher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRefresherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRefresherApplication.class, args);
+	}
+
+}
